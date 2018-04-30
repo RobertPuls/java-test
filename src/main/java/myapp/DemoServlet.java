5b935350236010aee70c2b5bf9d0e458d71d9293
Puls <cg3t@M-C02V114HHTDF.nordstrom.net>
2018-04-30 14:44:12 -0600

    Hello Ray

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -27,6 +27,6 @@ public class DemoServlet extends HttpServlet {
       throws IOException {
         System.out.println(req.getQueryString());
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
+    resp.getWriter().println("{ \"name\": \"Ray\" }");
   }
 }
